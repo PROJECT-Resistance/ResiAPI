129aa186eb36a17dfb39b176b90a948b32f35936
Lord Vertice <lordvertice333@gmail.com>
2021-01-19 18:24:19 +0100

    1.3.0
    Added basic auth

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth');
 const { promisify } = require('util');
 
 const initializeDatabase = require('./database');
+const config = require('./config.json');
+const users = config.users;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(basicAuth({ users, challenge: true }));
 
 const startServer = async () => {
     await initializeDatabase(app);
